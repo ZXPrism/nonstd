e4d96bf0c4ea9f1c5ca18291b5a6c333675d6f5d
ZXPrism <2897544036@qq.com>
2025-07-02 17:00:21 +0800

    test: add more testcases for nstd::forward

--- src/util/nstd_utility.h
+++ src/util/nstd_utility.h
@@ -5,12 +5,12 @@
 namespace nstd {
 
 template<typename Ty>
-constexpr Ty &&forward(remove_reference_t<Ty> &x) noexcept {
+constexpr Ty &&forward(remove_reference_t<Ty> &x) noexcept {  // overload accepts lvalue
 	return static_cast<Ty &&>(x);
 }
 
 template<typename Ty>
-constexpr Ty &&forward(remove_reference_t<Ty> &&x) noexcept {
+constexpr Ty &&forward(remove_reference_t<Ty> &&x) noexcept {  // overload accepts rvalue
 	static_assert(!is_lvalue_reference_v<Ty>, "attempting to forward a rvalue as a lvalue!");
 	return static_cast<Ty &&>(x);
 }
--- test/util/nstd_utility.cpp
+++ test/util/nstd_utility.cpp
@@ -5,6 +5,30 @@
 
 #include <utility>
 
+namespace test_forward {
+
+template<typename Ty>
+constexpr bool proc(Ty &) {
+	return true;
+}
+
+template<typename Ty>
+constexpr bool proc(Ty &&) {
+	return false;
+}
+
+template<typename Ty>
+constexpr bool wrapper_bad(Ty &&x) {
+	return proc<Ty>(x);  // bad: x is lvalue, so always call lvalue overload :(
+}
+
+template<typename Ty>
+constexpr bool wrapper_good(Ty &&x) {
+	return proc<Ty>(nstd::forward<Ty>(x));  // good: can decide overload based on Ty&&'s deduced type :)
+}
+
+}  // namespace test_forward
+
 TEST_CASE("forward") {
 	class A {
 	public:
@@ -23,6 +47,11 @@ TEST_CASE("forward") {
 	CHECK_EQ(nstd::forward<A &>(a).foo(), 0);
 	CHECK_EQ(nstd::forward<A>(A()).foo(), 1);
 
+	CHECK_EQ(nstd::forward<A &>(a).foo(), std::forward<A &>(a).foo());
 	CHECK_EQ(nstd::forward<A>(A()).foo(), std::forward<A>(A()).foo());
-	CHECK_EQ(nstd::forward<A>(A()).foo(), std::forward<A>(A()).foo());
+
+	CHECK(!test_forward::wrapper_good(42));
+	CHECK(test_forward::wrapper_good(a));
+	CHECK(test_forward::wrapper_bad(42));
+	CHECK(test_forward::wrapper_bad(a));
 }
