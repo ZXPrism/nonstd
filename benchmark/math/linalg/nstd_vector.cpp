53615290e888c74601ced914a984c01d4bc717e1
ZXPrism <2897544036@qq.com>
2025-07-07 17:31:21 +0800

    bench: add glm baseline for nstd::vector

--- benchmark/math/linalg/nstd_vector.cpp
+++ benchmark/math/linalg/nstd_vector.cpp
@@ -6,6 +6,7 @@
 
 #include <math/linalg/nstd_vector.h>
 
+#include <glm/glm.hpp>
 #include <Eigen/Dense>
 
 // bench_vector_normalize BEGINS
@@ -19,6 +20,11 @@ void BM_eigen_vector_normalize() {
 	ankerl::nanobench::doNotOptimizeAway(vec4f.normalized());
 }
 
+void BM_glm_vector_normalize() {
+	glm::vec4 vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+	ankerl::nanobench::doNotOptimizeAway(glm::normalize(vec4f));
+}
+
 TEST_CASE("bench_vector_normalize") {
 	auto bench = ankerl::nanobench::Bench();
 	bench.title("bench_vector_normalize")
@@ -28,6 +34,7 @@ TEST_CASE("bench_vector_normalize") {
 	    .relative(true);
 
 	bench.run("eigen / vector_normalize", BM_eigen_vector_normalize);
+	bench.run("glm / vector_normalize", BM_glm_vector_normalize);
 	bench.run("nonstd / vector_normalize", BM_nonstd_vector_normalize);
 }
 // bench_vector_normalize ENDS
@@ -43,6 +50,11 @@ void BM_eigen_vector_norm_squared() {
 	ankerl::nanobench::doNotOptimizeAway(vec4f.squaredNorm());
 }
 
+void BM_glm_vector_norm_squared() {
+	glm::vec4 vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+	ankerl::nanobench::doNotOptimizeAway(glm::dot(vec4f, vec4f));
+}
+
 TEST_CASE("bench_vector_norm_squared") {
 	auto bench = ankerl::nanobench::Bench();
 	bench.title("bench_vector_norm_squared")
@@ -52,6 +64,7 @@ TEST_CASE("bench_vector_norm_squared") {
 	    .relative(true);
 
 	bench.run("eigen / vector_norm_squared", BM_eigen_vector_norm_squared);
+	bench.run("glm / vector_norm_squared", BM_glm_vector_norm_squared);
 	bench.run("nonstd / vector_norm_squared", BM_nonstd_vector_norm_squared);
 }
 // bench_vector_norm_squared ENDS
@@ -67,6 +80,11 @@ void BM_eigen_vector_add() {
 	ankerl::nanobench::doNotOptimizeAway(vec4f_a + vec4f_b);
 }
 
+void BM_glm_vector_add() {
+	glm::vec4 vec4f_a(1.0f, 2.0f, 3.0f, 4.0f), vec4f_b(1.0f, 2.0f, 3.0f, 4.0f);
+	ankerl::nanobench::doNotOptimizeAway(vec4f_a + vec4f_b);
+}
+
 TEST_CASE("bench_vector_add") {
 	auto bench = ankerl::nanobench::Bench();
 	bench.title("bench_vector_add")
@@ -76,6 +94,7 @@ TEST_CASE("bench_vector_add") {
 	    .relative(true);
 
 	bench.run("eigen / vector_add", BM_eigen_vector_add);
+	bench.run("glm / vector_add", BM_glm_vector_add);
 	bench.run("nonstd / vector_add", BM_nonstd_vector_add);
 }
 // bench_vector_add ENDS
@@ -91,6 +110,11 @@ void BM_eigen_vector_dot() {
 	ankerl::nanobench::doNotOptimizeAway(vec4f_a.dot(vec4f_b));
 }
 
+void BM_glm_vector_dot() {
+	glm::vec4 vec4f_a(1.0f, 2.0f, 3.0f, 4.0f), vec4f_b(1.0f, 2.0f, 3.0f, 4.0f);
+	ankerl::nanobench::doNotOptimizeAway(glm::dot(vec4f_a, vec4f_b));
+}
+
 TEST_CASE("bench_vector_dot") {
 	auto bench = ankerl::nanobench::Bench();
 	bench.title("bench_vector_dot")
@@ -100,6 +124,7 @@ TEST_CASE("bench_vector_dot") {
 	    .relative(true);
 
 	bench.run("eigen / vector_dot", BM_eigen_vector_dot);
+	bench.run("glm / vector_dot", BM_glm_vector_dot);
 	bench.run("nonstd / vector_dot", BM_nonstd_vector_dot);
 }
 // bench_vector_dot ENDS
@@ -115,6 +140,11 @@ void BM_eigen_vector_cross() {
 	ankerl::nanobench::doNotOptimizeAway(vec3f_a.cross(vec3f_b));
 }
 
+void BM_glm_vector_cross() {
+	glm::vec3 vec3f_a(1.0f, 2.0f, 3.0f), vec3f_b(1.0f, 2.0f, 3.0f);
+	ankerl::nanobench::doNotOptimizeAway(glm::cross(vec3f_a, vec3f_b));
+}
+
 TEST_CASE("bench_vector_cross") {
 	auto bench = ankerl::nanobench::Bench();
 	bench.title("bench_vector_cross")
@@ -124,6 +154,7 @@ TEST_CASE("bench_vector_cross") {
 	    .relative(true);
 
 	bench.run("eigen / vector_cross", BM_eigen_vector_cross);
+	bench.run("glm / vector_cross", BM_glm_vector_cross);
 	bench.run("nonstd / vector_cross", BM_nonstd_vector_cross);
 }
 // bench_vector_cross ENDS
